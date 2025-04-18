8451055aeba2309444b782f3be2cc05f7d304603
Samuel Diaz <90120898+samuelgiorno@users.noreply.github.com>
2025-04-18 21:43:28 +0200

    Update app.js
    cambio del null

--- src/js/app.js
+++ src/js/app.js
@@ -33,19 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name || "Lucy"} ${variables.lastName ||
-    "Boilett"}</h1>
-          <h2>${variables.role || "Web Developer"}</h2>
-          <h3>${variables.city || "Miami"}, ${variables.country || "USA"}</h3>
-          <ul class="${variables.socialMediaPosition || "position-right"}">
-            <li><a href="https://twitter.com/${variables.twitter ||
-              "4geeksacademy"}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github ||
-              "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/${variables.linkedin ||
-              "4geeksacademy"}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram ||
-              "4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Samuel"} ${variables.lastName || "Diaz"}</h1>
+          <h2>${variables.role || ""}</h2>
+          <h3>${variables.city || ""}, ${variables.country || ""}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${variables.twitter ? variables.twitter : "4geeksacademy"}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ? variables.github : "4geeksacademy"}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin ? variables.linkedin : "school/4geeksacademy"}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : "4geeksacademy"}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -59,21 +54,23 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://media.licdn.com/dms/image/v2/D4D16AQFeDqYPbr-wUA/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1707401948667?e=1750291200&v=beta&t=0gSr8OLDtF2BpDVVlBFhyQzQDGuvAfRKtvULKNV9Pv0",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media.licdn.com/dms/image/v2/D4D03AQEAWk0A-8zRzw/profile-displayphoto-shrink_400_400/B4DZXUMAhSHAAk-/0/1743021673804?e=1750291200&v=beta&t=XqWc2HBwlYIFdiEcLu-HZTHEO9W2DqvD0yudIHSfu2o",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "4geeksacademy",
+    github: "4geeksacademy",
+    linkedin: "4geeksacademy",
+    instagram: "4geeksacademy",
+    name: "Samuel",
+    lastName: "Diaz",
+    role: "Web Developer",
+    country: "Valencia",
+    city: "España"
   };
   render(window.variables); // render the card for the first time
 
